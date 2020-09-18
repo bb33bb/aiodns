9fe09111205536d908ea0c6b78910fbdde242b0d
Connection Refused <cutecr@pm.me>
2020-09-18 18:07:09 +0800

    Fix CI

--- scripts/CI.ps1
+++ scripts/CI.ps1
@@ -3,7 +3,7 @@ $Env:GOROOT_FINAL="/usr"
 $Env:CGO_ENABLED=1
 $Env:GOOS="windows"
 $Env:GOARCH="amd64"
-go build -a -trimpath -buildmode=c-shared -asmflags "-s -w" -ldflags "-s -w" -gcflags "-l=4" -o aiodns.bin
+go build -a -trimpath -buildmode=c-shared -asmflags "-s -w" -ldflags "-s -w" -gcflags "-l=4" -o release/aiodns.bin
 if (! $?) { exit 1 }
 
 exit 0
