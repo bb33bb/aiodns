8307618152796af5d89f77adcf1174613ac2a33f
Connection Refused <cutecr@pm.me>
2021-03-26 18:49:03 +0800

    Update BUILD.ps1

--- scripts/BUILD.ps1
+++ scripts/BUILD.ps1
@@ -1,14 +1,11 @@
-Set-Location ..
-
-$Env:CGO_ENABLED="1"
 $Env:GOROOT_FINAL="/usr"
+$Env:CGO_ENABLED="1"
 
 $Env:GOOS="windows"
 $Env:GOARCH="amd64"
-go build -a -trimpath -buildmode=c-shared -asmflags "-s -w" -ldflags "-s -w" -gcflags "-l=4" -o aiodns.bin
+go build -a -trimpath -buildmode=c-shared -asmflags "-s -w" -ldflags "-s -w" -o aiodns.bin
 if (! $?) { exit 1 }
 
 upx --ultra-brute aiodns.bin
 
-Set-Location scripts
 exit 0
